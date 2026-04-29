cc14f59c1bd117ba677e5a83d35112f6c3ee33b3
Alex Kunev <akkunev@gmail.com>
2026-04-29 12:07:24 +0300

    Implement GitHub Pages deployment workflow and add configuration management

--- scripts/post-outputs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PROJECT=$1
-OUTPUTS=$(terraform -chdir=stacks/$STACK_TYPE output -json)
-az storage blob upload \
-  --account-name sttfstateplatform \
-  --container-name provisioner-outputs \
-  --name "$PROJECT-$ENVIRONMENT.json" \
-  --data "$OUTPUTS" \
-  --overwrite
